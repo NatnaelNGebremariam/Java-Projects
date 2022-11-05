baf7b7a209eb4ba87088766c5ad53407adbd4bc8
NatnaelNGebremariam <114710212+NatnaelNGebremariam@users.noreply.github.com>
2022-11-05 19:04:05 -0400

    Add files via upload

---
+++ FlagsoftheWorld/src/studentCode/FlagMaker.java
@@ -0,0 +1,17 @@
+package studentCode;
+import java.awt.Color;
+import GridTools.MyGrid;
+
+public class FlagMaker {
+
+	public static void drawFlag(MyGrid grid, int countryCode) {
+		
+		/* Remove this comment and implement this method according to
+		 * the project specification.
+		 */
+		
+	}
+	
+
+	
+}
